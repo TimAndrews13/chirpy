2c40d3d2de046fd97183c0c25b13dd396cbd0036
TimAndrews13 <andrews.timothya@gmail.com>
2026-03-21 11:10:55 -0400

    Set up basic server, and add chirpy logo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TimAndrews13/chirpy
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	log.Printf("Serving on port: 8080\n")
+	log.Fatal(server.ListenAndServe())
+}
