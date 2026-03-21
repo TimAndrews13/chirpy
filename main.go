d5dcfb3562f9742f380a43508dd85b7596dc782b
TimAndrews13 <andrews.timothya@gmail.com>
2026-03-21 12:22:49 -0400

    Add Custom Hanlder for Readiness Check

--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import (
 	"net/http"
 )
 
+// Add Readiness Endpoint
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	//Create ServeMux
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	//Register Handler Funciton
+	mux.HandleFunc("/healthz", handlerReadiness)
 
+	//Register Handle
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	//Define Server Params
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
